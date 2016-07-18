2e717d71403db95a07086d7de60cc83889c3a357
Zach Liou <zach@trackrevenue.com>
2016-07-18 12:19:58 -0700

    Test DOM element retrieval via incremented ID names and class names

--- lab/lab.js
+++ lab/lab.js
@@ -14,25 +14,48 @@
     var input = {
         "header": "Hello!",
         "test": "return",
+        "desc": "This is a test!",
     };
 
     // Get DOM elements
     var header = document.getElementById("header");
     var test = document.getElementById("test");
 
-    // TEST: getting nonexistent id
-    var none = document.getElementById("thisiddoesnotexist");
-    console.log("test: (control)");
-    console.log(test);
-    console.log("none: ");
-    console.log(none);
-
     // Set element values
     header.innerHTML = input.header;
     test.innerHTML = input.test;
 
-    // Get file
-    //readTextFile("file://input.txt");
+
+    
+    /*
+     * Testing procedures go here
+     */
+    var elems = [];
+    var getId;
+    var i = 0;
+    var idBase = "mult";
+
+    getId = document.getElementById(idBase + i);
+    while (getId) {
+        console.log(getId);
+        //elems.push(getId);
+        getId.innerHTML = input.desc + " " + i + "0"; 
+        ++i;
+        getId = document.getElementById(idBase + i);
+    }
+
+    var hello = document.getElementsByClassName("hello ");
+    console.log(hello);
+    for (i = 0; i < hello.length; ++i) {
+        hello[i].innerHTML = input.desc + " " + i;
+    }
+    
+
+
+
+
+
+
 
     //
     //
